ee00cc65c71a20e5b865592a255adea9f31a2e68
30shanti <sblues0123@gmail.com>
2024-05-30 14:13:35 +0900

    js core 데이터타입

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,51 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin, name;
+
+name = 'john';
+
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = 'Earth';
+let username = 'ddong';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+
+let calcPriceOfProductByQuantity;
+
+// - 구매 제품 가격의 총 합
+
+let totalPriceOfProduct;
+
+// - 1년 기준 일(day)자 수
+
+const DAY_PER_YEAR = 365;
+
+// - 구매 결제 여부
+
+let isPayment = false;
+
+// - 구매 결제 내역
+
+let PaymentList;
+
+// - 브랜드 접두사
+
+const BRAND_PREFIX = 'Stussy';
+
+// - 오늘의 운세
+
+let fotuneToday = 'so good';
+
+// - 상품 정보
+
+const ProductInfo = '';
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
